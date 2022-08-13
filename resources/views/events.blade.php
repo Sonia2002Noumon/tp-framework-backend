5b8fd7b2a9c11971b95fab572062bb32e9f3ca7b
DejesuAll <kitoyievrane21112@gmail.com>
2022-08-14 00:16:09 +0100

    initialisation

---
+++ resources/views/events.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="even.css">
+</head>
+<body>
+<form action="/events" method="post">
+        <input type="text" name="name" placeholder="nom">
+        <input type="text" name="date" placeholder="date_dem">
+        <input type="text" name="date" placeholder="date_fin">
+        <textarea name="message" cols="30" rows="10" placeholder="description"></textarea>
+        <button>Enregistrer</button>
+    </form>
+</body>
+</html>
